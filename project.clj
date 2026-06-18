88f6898f5cb11b33c9a0878d169a456b6a162d04
Kyle Cesare <kcesare@gmail.com>
2026-06-17 20:00:20 -0600

    Add project.clj metadata

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject org.clojars.kylc/jank-build-pkg-config "0.1-SNAPSHOT"
+  :description "Build script helper for querying libraries via pkg-config"
+  :url "https://github.com/kylc/jank-build-pkg-config"
   :license {:name "MPL 2.0"
             :url  "https://www.mozilla.org/en-US/MPL/2.0/"}
   :source-paths ["src"])
